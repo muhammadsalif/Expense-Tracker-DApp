83470d3f4b58712ea31decbe564e0967636d3b93
Muhammad Salif <muhammad.salif@yahoo.com>
2020-10-10 02:22:22 +0500

    Hardcoded three components done.

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Balance() {
     return (
-        <div className="flex justify-around w-full border-2 border-black font-serif text-3xl text-blue-700">
+        <div className="flex justify-around w-full text-4xl">
             <h1>Your Balance</h1>
-            <p>$500</p>
+            <p className="font-serif">$500</p>
         </div>
     )
 }
--- src/components/IncomeExpense.jsx
+++ src/components/IncomeExpense.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 export default function IncomeExpense() {
     return (
-        <div>
-            IncomeExpense component
-        </div>
+        <div className="flex justify-around items-center w-full flex-col lg:flex-row py-10">
+            <div className="flex flex-row justify-between  mb-5 lg:mb-0"> 
+                  <h1 className ="text-4xl pr-4">
+                        Income 
+                      </h1>
+                 <p className="text-green-700 text-4xl font-serif">$500</p>
+            </div>
+            <div className="flex flex-row justify-between">
+                  <h1 className ="text-4xl pr-4">
+                     Expense 
+                   </h1>
+                 <p className="text-red-700 text-4xl font-serif">$400</p>
+            </div>
+    </div>
     )
 }
