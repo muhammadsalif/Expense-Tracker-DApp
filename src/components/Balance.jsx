630c326745ba0b9653910b08fd5506622c7ac27d
Muhammad Salif <muhammad.salif@yahoo.com>
2020-10-10 12:39:59 +0500

    Income expense component done.

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Balance() {
     return (
-        <div className="flex justify-around w-full text-4xl">
+        <div className="flex justify-around w-full text-4xl border-4 border-double border-orange-300">
             <h1>Your Balance</h1>
             <p className="font-serif">$500</p>
         </div>
--- src/components/IncomeExpense.jsx
+++ src/components/IncomeExpense.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function IncomeExpense() {
     return (
-        <div className="flex justify-around items-center w-full flex-col lg:flex-row py-10">
-            <div className="flex flex-row justify-between  mb-5 lg:mb-0"> 
+        <div className="flex justify-around border-4 items-center w-full flex-col lg:flex-row py-10">
+            <div className="flex flex-row px-8 bg-teal-200 py-5 lg:mb-0 border-double border-4 border-green-700 my-2 lg:my-0"> 
                   <h1 className ="text-4xl pr-4">
                         Income 
                       </h1>
                  <p className="text-green-700 text-4xl font-serif">$500</p>
             </div>
-            <div className="flex flex-row justify-between">
+            <div className="flex flex-row px-8 bg-red-200 py-5 border-double border-4 border-red-700">
                   <h1 className ="text-4xl pr-4">
                      Expense 
                    </h1>
