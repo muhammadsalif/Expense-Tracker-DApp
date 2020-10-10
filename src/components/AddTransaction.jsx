8f1a6a5e8b149be89f7f1197d0a875371f3d9f80
Muhammad Salif <muhammad.salif@yahoo.com>
2020-10-10 21:16:51 +0500

    AddTransaction done.

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 export default function AddTransaction() {
-    return (
+  return (
+    <>
+      <h1 className="pl-4 text-2xl w-full border-b-4 border-double border-current">
+        Add New Transaction
+      </h1>
+      <form className="px-2 w-full">
+        {/* First Group */}
         <div>
-            this is AddTransaction
+          <label className="ml-2 text-2xl" htmlFor="description">
+            Enter Description
+          </label>
+          <input
+            name="description"
+            type="text"
+            required
+            className="rounded w-full px-3 py-2 border border-gray-500 placeholder-gray-500 text-black-500"
+            placeholder="Enter Description"
+          />
         </div>
-    )
+        {/* Second Group */}
+        <div className="mt-2">
+          <label className="ml-2 text-2xl" htmlFor="amount">
+            Enter Amount
+          </label>
+          <input
+            name="amount"
+            type="text"
+            required
+            className="rounded w-full px-3 py-2 border border-gray-500 placeholder-gray-500 text-black-500"
+            placeholder="Enter Amount"
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-xl my-2 bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded"
+        >
+          Add Transaction
+        </button>
+      </form>
+    </>
+  );
 }
--- src/components/IncomeExpense.jsx
+++ src/components/IncomeExpense.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function IncomeExpense() {
-    return (
-        <div className="flex justify-around border-4 items-center w-full flex-col lg:flex-row py-10">
-            <div className="flex flex-row px-8 bg-teal-200 py-5 lg:mb-0 border-double border-4 border-green-700 my-2 lg:my-0"> 
-                  <h1 className ="text-4xl pr-4">
-                        Income 
-                      </h1>
-                 <p className="text-green-700 text-4xl font-serif">$500</p>
-            </div>
-            <div className="flex flex-row px-8 bg-red-200 py-5 border-double border-4 border-red-700">
-                  <h1 className ="text-4xl pr-4">
-                     Expense 
-                   </h1>
-                 <p className="text-red-700 text-4xl font-serif">$400</p>
-            </div>
+  return (
+    <div className="flex justify-around border-4 border-red-500 items-center w-full flex-col lg:flex-row py-5">
+      {/*Income div  */}
+      <div className="flex flex-row px-8 bg-teal-200 py-5 lg:mb-0 border-double border-4 border-green-700 my-2 lg:my-0">
+        <h1 className="text-4xl">Income</h1>
+        <p className="text-green-700 text-4xl font-serif rounded-full px-5 border border-current">
+          $500
+        </p>
+      </div>
+      {/*Income div  */}
+      <div className="flex flex-row px-8 bg-red-200 py-5 border-double border-4 border-red-700">
+        <h1 className="text-4xl">Expense</h1>
+        <p className="text-red-700 text-4xl font-serif">$400</p>
+      </div>
     </div>
-    )
+  );
 }
